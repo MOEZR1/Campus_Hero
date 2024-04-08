1f448bb11213be4134d82ac4783d125462ad73a0
MOEZR <mohammedalbahly206@gmail.com>
2024-04-08 23:05:19 +0100

    ready to deploy website

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,16 @@ app.use('/habit', habitRoutes);
 app.use('/api', passwordResetRoutes); 
 
 
+const path = require('path');
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+});
+
+
 
 
 const port = process.env.PORT || 5000;
