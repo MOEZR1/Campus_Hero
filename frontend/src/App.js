34a99d3585b3dfba31f61728494af50219af5a27
mohammedalbahly206@gmail.com <mohammedalbahly206@gmail.com>
2024-04-09 15:39:07 +0100

    app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import 'string.prototype.endswith'; 
+// ... rest of your React code
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
